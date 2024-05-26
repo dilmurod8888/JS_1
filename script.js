49e767f75d394f5bb99d9ae669c634eb78c7ba61
dilmurod8888 <dilmurodsalimjonov8888@gail.com>
2024-05-26 19:17:51 +0500

    JS_1

---
+++ script.js
@@ -0,0 +1,28 @@
+var name= prompt(" Ismingizni kiriting ")
+var surname = prompt(" Sizning faniliyangiz ")
+
+var miSol1 = prompt("Misolni yeching 23+25= ")
+var miSol2 = prompt("Misolni yeching 96-85= ")
+var miSol3 = prompt("Misolni yeching 23*25= ")
+var miSol4 = prompt("Misolni yeching 232/23= ")
+var miSol5 = prompt("Misolni yeching 143==93= ")
+var miSol6 = prompt("Misolni yeching 63%77= ")
+var miSol7  = prompt("true yoki false: 89>=29 = ")
+var miSol8  = prompt("true yoki false: 23===25 = ")
+var miSol9  = prompt("Misolni yeching 96!==47 = ")
+var boLe = prompt("true yoki false: null==undefined")
+
+console.log("Sizning ismingiz " + name)
+console.log("Sizning faniliyangiz " + surname )
+
+console.log("23+25 =" + (23+25) +" Sizni javobingiz = " + miSol1 );
+console.log("96-85 = "+ (96-85) +" Sizni javobingiz = " + miSol2 );
+console.log("23*25 = "+ (23*25) +" Sizni javobingiz = " + miSol3 );
+console.log("232/23 = "+ (232/23) +" Sizni javobingiz = " + miSol4 );
+console.log("143==93= "+ (143==93)+" Sizni javobingiz = " + miSol5 );
+console.log("63%77 = "+ (63%77) +" Sizni javobingiz = " + miSol6  );
+console.log("89>=29 = "+ (89>=29) + " Sizni javobingiz = " + miSol7  );
+console.log('23===25 = '+ (23===25) + " Sizni javobingiz = " + miSol8  );
+console.log('96!==47 = '+ (96!==47) + " Sizni javobingiz = " + miSol9  );
+console.log("null==undefined "+(null==undefined )+" Sizni javobingiz = " + boLe)
+alert("Etiboringiz uchun raxmat")
